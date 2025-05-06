8b0e6c9e6ea8cadf7be2cfbdc317b7dcae0a82ba
Jonathan Rojas <38105133+waflex@users.noreply.github.com>
2025-05-05 22:15:17 -0400

    cambios realizados en muchas cosas no recuerdo no hice commit x.x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },experimental: {
+  integrations: [tailwind()],experimental: {
     svg: true
+  },
+  output: "static",
+  build: {
+    assets: "_assets"
   }
 });
