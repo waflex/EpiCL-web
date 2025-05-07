bcb97c9d287a4155ee0b6e3eb44a3892fda0ef11
Jonathan Rojas <38105133+waflex@users.noreply.github.com>
2025-05-07 19:22:54 -0400

    agregados alias, cambios en templos, creados nuevos templos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,25 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-// https://astro.build/config
+import path from "path"; // <-- AÑADIDO
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-  },experimental: {
+    resolve: {
+      alias: {
+        '@components': path.resolve('./src/components'),
+        '@layouts': path.resolve('./src/layouts'),
+        '@pages': path.resolve('./src/pages'),
+        '@utils': path.resolve('./src/utils'),
+        '@styles': path.resolve('./src/css'),
+        '@assets': path.resolve('./src/assets'),
+        '@public': path.resolve('./public'),
+        '@Img': path.resolve('./src/Img'),
+      }
+    }
+  },
+  experimental: {
     svg: true
-  }
+  },
 });
