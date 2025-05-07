60dcd8fec228a967cfd2cc1cec694c948568e306
Jonathan Rojas <38105133+waflex@users.noreply.github.com>
2025-05-07 11:38:32 -0400

    Revert "cambios realizados en muchas cosas no recuerdo no hice commit x.x"
    This reverts commit 8b0e6c9e6ea8cadf7be2cfbdc317b7dcae0a82ba.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
+import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],experimental: {
+  vite: {
+    plugins: [tailwindcss()],
+  },experimental: {
     svg: true
-  },
-  output: "static",
-  build: {
-    assets: "_assets"
   }
 });
